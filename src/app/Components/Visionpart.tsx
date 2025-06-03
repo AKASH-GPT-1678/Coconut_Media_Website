45bba60ef0508fe227c7e82002f5e7c1f73a6076
AKASH-GPT-1678 <akashgpt5974@gmail.com>
2025-06-03 14:51:43 +0530

    next-config

--- src/app/Components/Visionpart.tsx
+++ src/app/Components/Visionpart.tsx
@@ -1,7 +1,49 @@
 import React from 'react'
+import Mystudio from '../testimonial/mystudio.webp'
+import Image from 'next/image'
+const Visionpart = () => {
+    return (
+        <div className='h-[600px] w-full border-2 border-black'>
+            <div className="h-[60%]  flex flex-row items-center justify-center p-3  bg-green-50 
+      " style={{ backgroundImage: `url(${'https://storage.googleapis.com/management_324/mystudio.webp'})` }}>
+                <div className='flex flex-row  items-center justify-center'>
+                    <div className='flex flex-col items-center justify-center '>
 
-export const Visionpart = () => {
-  return (
-    <div>Visionpart</div>
-  )
+                        <div className=' bg-green-600 h-26 w-26 flex items-center justify-center rounded-2xl'>
+
+                            <div className='border-4 border-white h-8 w-8 '>
+
+                            </div>
+
+                        </div>
+                        <div>
+                            <p className='uppercase text-white'>Frames shaped by intention and imagination</p>
+                            <p className='text-5xl text-white'>Vision in Motion</p>
+                        </div>
+
+
+                    </div>
+                </div>
+
+            </div>
+            <div className='h-[40%]' style={{ backgroundImage: `url(${'https://storage.googleapis.com/management_324/illusion.jpg'})` }}>
+
+
+                <div className='flex flex-row items-center justify-center gap-5 '>
+
+
+
+                    <Image src={Mystudio} alt="Image 1" width={300} height={300} className='-mt-20' />
+                    <Image src={Mystudio} alt="Image 1" width={300} height={300} className='-mt-20' />
+                    <Image src={Mystudio} alt="Image 1" width={300} height={300} className='-mt-20' />
+
+                </div>
+
+
+
+            </div>
+        </div>
+    )
 }
+
+export default Visionpart
--- src/app/testing/page.tsx
+++ src/app/testing/page.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import Infopart from '../Components/Infopart'
 import { KnowAboutUs } from '../Components/KnowAboutUs'
 import Footer from '../Components/Footer'
+import Visionpart from '../Components/Visionpart'
 const page = () => {
   return (
     <div>
+        <Visionpart/>
 
 
     </div>
