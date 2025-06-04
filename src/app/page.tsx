1958c5640633e36b6ed5275c71def98244ab310c
AKASH-GPT-1678 <akashgpt5974@gmail.com>
2025-06-04 13:09:53 +0530

    Mostly completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,21 @@ import { Header } from "./Components/Header";
 import { BlogView } from "./Components/BlogView";
 import { Footer1 } from "./Components/Footer1";
 import Footer from "./Components/Footer";
+import { KnowAboutUs } from "./Components/KnowAboutUs";
+import Visionpart from "./Components/Visionpart";
+import Infopart from "./Components/Infopart";
 export default function Home() {
   return (
-      <div>
-      <Header/>
-      <BlogView/>
-      <Footer1/>
-      <Footer/>
+    <div>
+      <Header />
 
-      </div>
+      <KnowAboutUs />
+      <Infopart />
+      <Visionpart />
+      <BlogView />
+      <Footer1 />
+      <Footer />
+
+    </div>
   );
 }
