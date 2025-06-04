6c7af582c23b04f7832c924e3c69beec2c95702a
AKASH-GPT-1678 <akashgpt5974@gmail.com>
2025-06-04 17:34:33 +0530

    contact page ready

---
+++ src/app/blogs/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { BlogView } from '../Components/BlogView'
+const page = () => {
+  return (
+    <div>
+        <BlogView/>
+      
+    </div>
+  )
+}
+
+export default page
